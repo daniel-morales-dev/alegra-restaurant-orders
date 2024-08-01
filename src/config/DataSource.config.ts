caa8e54847e854525dea611eb6177ef41b16c1ea
Daniel Morales <mralejo99@gmail.com>
2024-08-01 14:31:55 -0500

    fix: fixed routes models and migrations

--- src/config/DataSource.config.ts
+++ src/config/DataSource.config.ts
@@ -2,14 +2,16 @@ import { DataSource } from "typeorm";
 import { resolve } from "path";
 import Container from "typedi";
 
+console.log("MIS MODELOS", resolve(__dirname, "../models/*{.ts,.js}"));
+
 export const AppDataSourceMongoDB = new DataSource({
   type: "mongodb",
   url: process.env.MONGODB_HOST,
   useUnifiedTopology: true,
   useNewUrlParser: true,
   retryWrites: true,
-  entities: [resolve("src/models/*{.ts,.js}")],
-  migrations: [resolve("src/migrations/*{.ts,.js}")],
+  entities: [resolve(__dirname, "../models/*{.ts,.js}")],
+  migrations: [resolve(__dirname, "../migrations/*{.ts,.js}")],
   synchronize: false,
   logging: true,
 });
